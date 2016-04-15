693c5ffc41808f255fb5f51c80c9767fa49070c4
Dean Hetherington <deanhet@gmail.com>
2016-04-15 14:05:37 +0100

    Update test6_spec.js

--- test/test6_spec.js
+++ test/test6_spec.js
@@ -4,7 +4,7 @@ import 'babel-polyfill'
 /*
 Modify the contents of 'app/test6.js' and add 3 new classes - English, French and Spanish that extend the Greeting class.
 
-Override the "sayHello" method to say hello in teh correct language
+Override the "sayHello" method to say hello in the correct language
 */
 
 import {English, French, Spanish} from '../app/test6';
